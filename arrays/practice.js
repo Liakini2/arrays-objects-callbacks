abba1244c366a47886d2b7f24b968d1bdb33a987
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-01-07 14:12:34 -0700

    arrays done

--- arrays/practice.js
+++ arrays/practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+const first = (arr) =>{
+  return arr[0]
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +36,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+const last = (arr) =>{
+  return arr[2]
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+const looper = (family) =>{
+  for (let i = 0; i < family.length; i++){
+  alert(family[i])
+  } 
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -64,6 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+const reversedLooper = (letters) =>{
+  for (let i = letters.length - 1; i >= 0; i--){
+      alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,11 +92,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
-
-
-
+const evenFinder = (nums) =>{
+  let evens = []
+    for (let i = 0; i <= nums.length; i++){
+      if(nums[i] % 2 === 0){
+        evens.push(nums[i])
+      }
+    }
+    return evens
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////

1d9dea6ebce3c43614463159cfcf65adf7f2ef51
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-01-07 14:44:02 -0700

    halfway point for callbacks

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+const first = (arr, cb) =>{
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -39,7 +41,6 @@ first(names, function(firstName){
 // Do not edit the code above.
 
 
-
 ////////// PROBLEM 2 //////////
 
 /*
@@ -47,7 +48,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+const last = (arr, cb) =>{
+  cb(arr)
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -57,7 +60,6 @@ last(names, function(lastName){
 // Do not edit the code above.
 
 
-
 ////////// PROBLEM 3 //////////
 
 /*
@@ -65,15 +67,15 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+const multiply = (num1, num2, cb) =>{
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 4 //////////
 
